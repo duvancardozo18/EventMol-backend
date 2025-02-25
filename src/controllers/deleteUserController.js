0e6ddf0092e3a1361eca17a19bf9c5c421f8b7a8
Diego Castro <castrodiego008@gmail.com>
2025-02-24 22:07:09 -0500

    Restablecer contraseña + funcion temporal postgress

---
+++ src/controllers/deleteUserController.js
@@ -0,0 +1,4 @@
+export const deleteUser = (req, res) => {
+    res.status(501).json({ mensaje: 'Funcionalidad en construcción.' });
+  };
+  
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { getUsers, createUser, verifyEmail, loginUser } from '../controllers/userController.js';
+import { requestPasswordReset, resetPassword } from '../controllers/newPasswordController.js';
+import { deleteUser } from '../controllers/deleteUserController.js';
+
 
 const router = Router();
 
@@ -7,6 +10,11 @@ router.get('/users', getUsers);
 router.post('/users', createUser);
 router.get('/verify-email/:token', verifyEmail);
 router.post('/login', loginUser);
+
+//diego:
+router.post('/request-password-reset', requestPasswordReset);
+router.post('/reset-password', resetPassword);
+router.delete('/users/:id_user', deleteUser);
 
 
 export default router;
