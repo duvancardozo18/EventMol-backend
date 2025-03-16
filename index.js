3f7a146e641c6d9cdb72fcb2cdd90be0cd7ecb00
Diego Castro <castrodiego008@gmail.com>
2025-03-15 22:19:40 -0500

    CRUD Participants

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import permissionsRoutes from './src/routes/permissionRoutes.js';
 import invitationRoutes from './src/routes/invitationRoutes.js';
 import statisticsRoutes from './src/routes/statisticsRoutes.js';
 import resourceRoutes from './src/routes/resourceRoutes.js';
-import eventResourceRoutes from './src/routes/eventResourceRoutes.js'
+import eventResourceRoutes from './src/routes/eventResourceRoutes.js';
+import participantsRoutes from './src/routes/participantsRoutes.js';
 
 // 1. Cargar variables de entorno
 dotenv.config();
@@ -46,6 +47,7 @@ app.use('/api', invitationRoutes);
 app.use('/api', statisticsRoutes);
 app.use('/api', resourceRoutes);
 app.use('/api', eventResourceRoutes);
+app.use('/api', participantsRoutes);
 
 // 6. Ruta inicial para verificar el servidor
 // app.get('/', (req, res) => {
