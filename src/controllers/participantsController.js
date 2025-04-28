5194c4d7f082589b1a194dbdef021e4de1a900d9
Minirick <duvanestivencardozo@gmail.com>
2025-04-27 22:22:31 -0500

    feat: apellido del participante de un evento

--- src/controllers/participantsController.js
+++ src/controllers/participantsController.js
@@ -119,7 +119,7 @@ import {
   
       // Obtener los participantes inscritos en el evento
       const result = await pool.query(
-        `SELECT p.id_participants, p.user_id, u.name AS user_name, u.email, ps.status_name 
+        `SELECT p.id_participants, p.user_id, u.name AS user_name, u.last_name AS user_last_name, u.email, ps.status_name 
          FROM participants p
          JOIN users u ON p.user_id = u.id_user
          JOIN participant_status ps ON p.participant_status_id = ps.id_participant_status
@@ -133,4 +133,5 @@ import {
       res.status(500).json({ message: "Error al obtener participantes del evento", error });
     }
   };
+  
   
